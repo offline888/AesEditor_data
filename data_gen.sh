4b9ad8164f3c0694b209c4b94d835271191a11ec
zach.duan <zach.duan@outlook.com>
2025-11-17 14:27:17 +0800

    update for multi gpu parallel

--- data_gen.sh
+++ data_gen.sh
@@ -1,18 +1,16 @@
 
-mkdir data
-cd data
-python ../download/download_images.sh
-mv imgs HQ
-cd ..
+part=0
+
+python download/download_images.py --part 4000-${part} --output_dir data/HQ/part${part}
 
 
-python scripts/add_distortion_sd.py --reference_dir data/HQ --distortion_dir data/LQ_S --json_path data/sd.json
+python scripts/add_distortion_sd.py --reference_dir data/HQ/part${part} --distortion_dir data/LQ_S --json_path data/sd_part${part}.json &
 
-python scripts/add_distortion_md.py --reference_dir data/HQ --distortion_dir data/LQ_M --json_path data/md.json
+python scripts/add_distortion_md.py --reference_dir data/HQ/part${part} --distortion_dir data/LQ_M --json_path data/md_part${part}.json
 
-
+wait
 
 # pip install transformers==4.57 # >=4.57
-python scripts/gen_instruction_sd.py --meta_json data/sd.json --save_json data/sd_instructions.jsonl --model-path Qwen/Qwen3-VL-8B-Instruct
+CUDA_VISIBLE_DEVICES=$((${part}*2)) python scripts/gen_instruction_sd.py --meta_json data/sd_part${part}.json --save_json data/sd_instructions_part${part}.jsonl --model-path Qwen/Qwen3-VL-8B-Instruct &
 
-python scripts/gen_instruction_md.py --meta_json data/md.json --save_json data/md_instructions.jsonl --model-path Qwen/Qwen3-VL-8B-Instruct
+CUDA_VISIBLE_DEVICES=$((${part}*2+1)) python scripts/gen_instruction_md.py --meta_json data/md_part${part}.json --save_json data/md_instructions_part${part}.jsonl --model-path Qwen/Qwen3-VL-8B-Instruct
--- download/download_images.py
+++ download/download_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
-
+import argparse
 import requests
 
 
@@ -20,15 +20,26 @@ def download(url, target_dir):
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Download images from URLs")
+    parser.add_argument("--urls_path", type=str, default="download/urls.txt", help="Path to the URLs file")
+    parser.add_argument("--output_dir", type=str, default="data/HQ", help="Path to the output directory")
+    parser.add_argument("--part", type=str, default="4-0", help="Part of the URLs file to download, e.g. 4-0 is the 0th part of the 4 parts")
+    args = parser.parse_args()
+
     script_dir = Path(__file__).parent
-    urls_path = script_dir / "urls.txt"
-    output_dir = script_dir.parent / "imgs"
+    urls_path = Path(args.urls_path)
+    output_dir = Path(args.output_dir)
 
     try:
         urls = [line.strip() for line in urls_path.read_text(encoding="utf-8").splitlines() if line.strip()]
     except FileNotFoundError:
         print(f"找不到 {urls_path}")
         sys.exit(1)
+    
+    parts, index = args.part.split("-")
+    parts = int(parts)
+    index = int(index)
+    urls = urls[index::parts]
 
     for index, url in enumerate(urls, 1):
         try:
